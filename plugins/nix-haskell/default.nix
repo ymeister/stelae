d582c952f9f8d922183fa90af8ae169529128432
ymeister <47071325+ymeister@users.noreply.github.com>
2026-03-20 09:39:50 +0900

    Use __functor for nix-haskell plugin export
    nix-haskell returns a function, not an attrset, so the //
    operator fails. Wrap it with __functor to make it both
    callable and carry src/nix-thunk attributes.

--- plugins/nix-haskell/default.nix
+++ plugins/nix-haskell/default.nix
@@ -4,4 +4,4 @@ let nix-haskell = import ./thunk { inherit system; };
     nix-thunk = (nix-haskell {}).config.importing.nix-thunk;
     src = nix-thunk.thunkSource ./thunk;
 
-in nix-haskell // { inherit src nix-thunk; }
+in { __functor = _: nix-haskell; inherit src nix-thunk; }
