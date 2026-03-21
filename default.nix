ebfbb8257787c79cf86f9a7dc12561561a78613c
ymeister <47071325+ymeister@users.noreply.github.com>
2026-03-21 13:25:36 +0900

    Inline mapSubdirectories to remove nix-thunk dependency

--- default.nix
+++ default.nix
@@ -1,5 +1,10 @@
 { system ? builtins.currentSystem }:
 
-let nix-thunk = import ./components/nix-thunk { inherit system; };
+let mapSubdirectories = f: dir:
+      let entries = builtins.readDir dir;
+      in builtins.mapAttrs (name: _: f (dir + "/${name}"))
+        (builtins.listToAttrs
+          (builtins.filter ({ name, ... }: let t = entries.${name}; in t == "directory" || t == "symlink")
+            (map (name: { inherit name; value = null; }) (builtins.attrNames entries))));
 
-in nix-thunk.mapSubdirectories (p: import p { inherit system; }) ./components
+in mapSubdirectories (p: import p { inherit system; }) ./components
